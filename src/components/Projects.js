580885afc2e37e92db1ec3b194c5c0438e537156
Ahmed <ahmadaboalua@gmail.com>
2024-07-27 12:44:10 -0700

    update3

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -132,7 +132,6 @@ export const Projects = () => {
                                             )
                                         })  
 
-                                        
                                          }
                                 </Row>
                             </Tab.Pane> 

814d8d2509de79131549628ae5846b60f723434d
AhmedAlhawa <ahmadalhawa85@gmail.com>
2025-11-18 19:58:22 -0800

    Updates

---
+++ precache-manifest.5236b02e3a80143fdd6f871ecc39a518.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62a426803be58df5aa523fdf0d6b73d",
+    "url": "/MyProtofolio/index.html"
+  },
+  {
+    "revision": "40a0bfad81c1ae18af9f",
+    "url": "/MyProtofolio/static/css/2.33197dd3.chunk.css"
+  },
+  {
+    "revision": "33c2f6dbff84057529e5",
+    "url": "/MyProtofolio/static/css/main.234dcf8f.chunk.css"
+  },
+  {
+    "revision": "40a0bfad81c1ae18af9f",
+    "url": "/MyProtofolio/static/js/2.69749aed.chunk.js"
+  },
+  {
+    "revision": "1a3dcad1b33a0e5a5bf94e2599b1257f",
+    "url": "/MyProtofolio/static/js/2.69749aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f155fdfd6e44a11db587",
+    "url": "/MyProtofolio/static/js/3.2dab1e5c.chunk.js"
+  },
+  {
+    "revision": "33c2f6dbff84057529e5",
+    "url": "/MyProtofolio/static/js/main.14d737ae.chunk.js"
+  },
+  {
+    "revision": "d86ab967c6cc74f10d56",
+    "url": "/MyProtofolio/static/js/runtime-main.af60aa08.js"
+  },
+  {
+    "revision": "5cfec2737bc3e106453e6ab5479463da",
+    "url": "/MyProtofolio/static/media/Agile.5cfec273.jpg"
+  },
+  {
+    "revision": "7ddd2e4433d5a09aceded56ce387b057",
+    "url": "/MyProtofolio/static/media/CSS.7ddd2e44.jpg"
+  },
+  {
+    "revision": "be0d54293c21cd1a7c34c5a538d1a57f",
+    "url": "/MyProtofolio/static/media/Dad-Jokes.be0d5429.png"
+  },
+  {
+    "revision": "c113da5ea1e7d187e7746029dc9baf9a",
+    "url": "/MyProtofolio/static/media/ExpandingCard.c113da5e.png"
+  },
+  {
+    "revision": "81add435823a4e5f9a9448fca90d07cc",
+    "url": "/MyProtofolio/static/media/Landing-Page.81add435.png"
+  },
+  {
+    "revision": "c198c6e5266d0149fd7140c088720257",
+    "url": "/MyProtofolio/static/media/MateriaUi.c198c6e5.jpg"
+  },
+  {
+    "revision": "2e4f0e9aeec7ab6580fd53eabac75747",
+    "url": "/MyProtofolio/static/media/Next.2e4f0e9a.jpg"
+  },
+  {
+    "revision": "96200949df34d77936efd528790a7414",
+    "url": "/MyProtofolio/static/media/RotatingNavigation.96200949.png"
+  },
+  {
+    "revision": "f2b6e36d3cdd47569cedc0fc0b28ded5",
+    "url": "/MyProtofolio/static/media/Tic-Tac-Toe.f2b6e36d.png"
+  },
+  {
+    "revision": "0b841b3fd2c95b701446e8d0e3e40ea1",
+    "url": "/MyProtofolio/static/media/VerticalSlider.0b841b3f.png"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/MyProtofolio/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "9a57748540a4b4419274e486b1f8c93f",
+    "url": "/MyProtofolio/static/media/chat.9a577485.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/MyProtofolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "d00b957cc0ae715acc9318bbea5747a6",
+    "url": "/MyProtofolio/static/media/contact-img.d00b957c.svg"
+  },
+  {
+    "revision": "b7ed1abb47bace747fb2643443635276",
+    "url": "/MyProtofolio/static/media/countries.b7ed1abb.png"
+  },
+  {
+    "revision": "8d442d4436da81083e7a7ce1b26cf8c2",
+    "url": "/MyProtofolio/static/media/cruddapp.8d442d44.png"
+  },
+  {
+    "revision": "d6e64ac196271ec511dcf84bd2fdc5bf",
+    "url": "/MyProtofolio/static/media/ecommerce.d6e64ac1.png"
+  },
+  {
+    "revision": "ac0de312ffa7ad914457ea8c90a9db8d",
+    "url": "/MyProtofolio/static/media/ff.ac0de312.png"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/MyProtofolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "90610e7f10980cb25dbbf49a3feb29c4",
+    "url": "/MyProtofolio/static/media/github.90610e7f.svg"
+  },
+  {
+    "revision": "e8257f2f5e202f185a1e7c78d8728dc2",
+    "url": "/MyProtofolio/static/media/header-img.e8257f2f.svg"
+  },
+  {
+    "revision": "7dc0c0da332da24bba20cceef6f87e42",
+    "url": "/MyProtofolio/static/media/html1.7dc0c0da.jpg"
+  },
+  {
+    "revision": "99af1bf54ae6962c5cb49fb23da76071",
+    "url": "/MyProtofolio/static/media/html5.99af1bf5.jpg"
+  },
+  {
+    "revision": "7ee9e2734190c25b72c464e3e053a324",
+    "url": "/MyProtofolio/static/media/js3.7ee9e273.jpg"
+  },
+  {
+    "revision": "731eff2abde5802c252eb4401e7b0414",
+    "url": "/MyProtofolio/static/media/logo.731eff2a.svg"
+  },
+  {
+    "revision": "f1a66e0601a6f6bb07b21547ac3f41fa",
+    "url": "/MyProtofolio/static/media/movieapp.f1a66e06.png"
+  },
+  {
+    "revision": "32152ae1361134bfa5d84d952664f94b",
+    "url": "/MyProtofolio/static/media/nav-icon1.32152ae1.svg"
+  },
+  {
+    "revision": "67f6d99ca315b05b039ac6a19210436e",
+    "url": "/MyProtofolio/static/media/nav-icon2.67f6d99c.svg"
+  },
+  {
+    "revision": "73725d64af982377e24b4dd8b555e896",
+    "url": "/MyProtofolio/static/media/nav-icon3.73725d64.svg"
+  },
+  {
+    "revision": "db984dfc680d1566d0c5971d377b8948",
+    "url": "/MyProtofolio/static/media/prayerTimings.db984dfc.png"
+  },
+  {
+    "revision": "0913016aa6013009920d63d0e7e91bfd",
+    "url": "/MyProtofolio/static/media/problems solving.0913016a.jpg"
+  },
+  {
+    "revision": "18547613b700a853428ca20febdb6e80",
+    "url": "/MyProtofolio/static/media/react2.18547613.jpg"
+  },
+  {
+    "revision": "f8db68d5da3534a3c3ced1fbfeac788c",
+    "url": "/MyProtofolio/static/media/redux.f8db68d5.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/MyProtofolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/MyProtofolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/MyProtofolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "a12cf4227f0c0b46047ac37a77f56095",
+    "url": "/MyProtofolio/static/media/shadcn.a12cf422.png"
+  },
+  {
+    "revision": "8b3354f9ca6685ce0ab63288e113ef5c",
+    "url": "/MyProtofolio/static/media/state1.8b3354f9.jpg"
+  },
+  {
+    "revision": "debc15b29c0f1f80f4bd0ce3e2152ba3",
+    "url": "/MyProtofolio/static/media/todolist.debc15b2.png"
+  },
+  {
+    "revision": "a6fd8057d6b6a080de2c04f116866d4e",
+    "url": "/MyProtofolio/static/media/uml3.a6fd8057.jpg"
+  }
+]);
